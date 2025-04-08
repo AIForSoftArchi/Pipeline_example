dd240e5ee374af87e19d8bb1494761c2b4d3987c
Casper2411 <ccaassppeerr2411@gmail.com>
2025-04-08 14:42:33 +0200

    Added comment

--- src/Application/Models/CustomerDTO.cs
+++ src/Application/Models/CustomerDTO.cs
@@ -1,8 +1,9 @@
 namespace Application.Models
 {
+    // Here is the CustomerDTO
     public class CustomerDTO
     {
-        
+
         public int Id { get; set; }
         public string Name { get; set; }
     }
