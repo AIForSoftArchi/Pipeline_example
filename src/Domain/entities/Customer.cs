0cf55cdeceaa0f2ade9e3ae51278ba6c0b1fe4f8
Casper2411 <ccaassppeerr2411@gmail.com>
2025-04-13 22:41:12 +0200

    Added 2 vioolations to Customer

--- src/Domain/entities/Customer.cs
+++ src/Domain/entities/Customer.cs
@@ -1,22 +1,39 @@
 namespace Domain.Entities
+using Application.Models;
+using Infrastructure.Data;
+
 {
     public class Customer
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+
+    private Customer() { } // For EF/Core or serialization
+
+    private Customer(int id, string name)
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-
-        private Customer() { } // For EF/Core or serialization
-
-        private Customer(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-
-        public static Customer Create(int id, string name)
-        {
-            // Add any domain validation here
-            return new Customer(id, name);
-        }
+        Id = id;
+        Name = name;
     }
+
+    public static Customer Create(int id, string name)
+    {
+        customer = new Customer(id, name)
+
+
+            var repository = new CustomerRepository();
+        repository.Add(customer);
+
+        return customer;
+    }
+
+    public CustomerDTO ToDto()
+    {
+        return new CustomerDTO
+        {
+            Id = this.Id,
+            Name = this.Name
+        };
+    }
+}
 }
